840df76a2ed8d2816143affe42e536b3f4dee9f3
Peter Tonner <peter.tonner@nist.gov>
2021-03-11 18:37:50 -0500

    udpate tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,6 +5,18 @@ from lantern.dataset import CsvDataset, Dataset, Tokenizer
 
 
 @pytest.fixture(scope="session")
+def single_phenotype_csv_file(tmpdir_factory):
+
+    df = pd.DataFrame(
+        {"substitutions": ["a1b", "c2d"], "phenotype": [0.0, 1.0], "error": [0.1, 0.2],}
+    )
+
+    filename = str(tmpdir_factory.mktemp("data").join("data.csv"))
+    df.to_csv(filename)
+    return filename
+
+
+@pytest.fixture(scope="session")
 def multiple_phenotype_csv_file(tmpdir_factory):
 
     df = pd.DataFrame(
@@ -20,6 +32,24 @@ def multiple_phenotype_csv_file(tmpdir_factory):
     filename = str(tmpdir_factory.mktemp("data").join("data.csv"))
     df.to_csv(filename)
     return filename
+
+
+def test_single_phenotype(single_phenotype_csv_file):
+
+    ds = CsvDataset(single_phenotype_csv_file)
+    assert ds.D == 1
+    assert ds.p == 2
+
+    ds = CsvDataset(single_phenotype_csv_file, errors=["error"])
+    assert ds.D == 1
+    assert ds.p == 2
+
+    # wrong error columns
+    with pytest.raises(ValueError):
+        ds = CsvDataset(multiple_phenotype_csv_file, errors=["e1"])
+
+    with pytest.raises(ValueError):
+        ds = CsvDataset(multiple_phenotype_csv_file, errors=["errors", "other"])
 
 
 def test_multiple_phenotypes(multiple_phenotype_csv_file):
